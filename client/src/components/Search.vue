de5b2e05a2fe7487835603f1096ac6433fb97b8f
charathmathew <42915749+charathmathew@users.noreply.github.com>
2020-07-17 19:01:02 -0400

    Vue.js frontend client application

---
+++ client/src/components/Search.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="search">
+        <h1>Track Player Stats</h1>
+        <form v-on:submit.prevent="onSubmit">
+            <div class="form-group">
+                <label for="platform">Platform</label>
+                <select name="platform" id="platform" v-model="platform">
+                    <option value="psn">Playstation</option>
+                    <option value="xbl">Xbox</option>
+                    <option value="origin">Origin</option>
+                </select>
+            </div>
+        <div class="form-group">
+            <label for="gamertag">Gamertag</label>
+            <input type="text" name="text" v-model="gamertag" id="gamertag" placeholder="Origin ID, Xbox Live gamertag or PSN ID" />
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Submit" class="btn" />
+        </div>
+        </form>        
+    </section>
+</template>
+
+<script>
+export default{
+    name: "Search",
+    data() {
+        return {
+            platform: "origin",
+            gamertag: ""
+        }
+    },
+    beforeCreate(){
+        document.body.className = "body-bg-image"
+    },
+    methods: {
+        onSubmit() {
+            if(!this.gamertag) {
+                this.$toasted.show("Please enter a gamertag", {
+                    duration: 2000    
+                });
+            }
+            else{
+                this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
